fde5efe8d9276bd476e5c47cb0c8c01643c6b1cc
João Vitor <vitorribeiro9454@gmail.com>
2023-06-28 16:25:02 -0300

    removendo if que não funcionou

--- assets/people.js
+++ assets/people.js
@@ -9,74 +9,72 @@ function handleCredentialResponse(response) {
     verifiedEmail.textContent = data.email_verified
     picture.setAttribute("src", data.picture)
 
-    if (typeof data.name != "undefined"){
-        var URL_BASE = "http://localhost:8080/"
-
-        function saveBoletim(){
-            //captura os dados do form, já colocando como um JSON
-            dados = $('#tipo_boletim, #cep_boletim, #cidade_boletim, #estado_boletim,#logradouro_boletim, #bairro_boletim, #desc_boletim, #previsao_boletim').serializeJSON();
-            dados['token_user'] = data.name;
-
-            console.log(dados);
-
-            //envia para o backend
-            $.ajax(URL_BASE+"boletim",{
-                data:JSON.stringify(dados),
-                method:'post',
-                contentType: "application/json",
-            }).done(function(res) {
-                console.log(res);
-            })
-            .fail(function(res) {
-                console.log(res);
-            });  
-
-        }
-
-        function saveProblema(){
-            //captura os dados do form, já colocando como um JSON
-            dados = $('#tipo_problema, #cep_problema, #cidade_problema, #estado_problema,#logradouro_problema, #numero_rua_problema, #bairro_problema, #desc_problema').serializeJSON();
-            dados['token_user'] = data.name;
-
-            console.log(dados);
-
-            //envia para o backend
-            $.ajax(URL_BASE+"problema",{
-                data:JSON.stringify(dados),
-                method:'post',
-                contentType: "application/json",
-            }).done(function(res) {
-
-                let table = $('#tableContent');
-                table.html("");
-                $(res._embedded.boletim).each(function(k,el){
-                    let boletim = el;
-                    tr = $(`<tr><td>Editar</td><td>Deletar</td></tr>`);
-                    table.append(tr);
-                })
-            
-            })
-            .fail(function(res) {
-                console.log(res);
-            });  
-
-        }
-
-    }
 }
-  
+
 google.accounts.id.initialize({
     client_id: "237096676007-vfap0beu1eusvicj0t7hvr85o2l7no6b.apps.googleusercontent.com", callback: handleCredentialResponse
 });
-  
+
 google.accounts.id.renderButton( 
     document.getElementById("buttonDiv"), {
-    theme: "filled_black",
+        theme: "filled_black",
     size: "large",
     type: "standard",
     shape: "pill",
     locale: "pt-BR",
     logo_alignment: "left",
 });
-  
+
 google.accounts.id.prompt(); // also display the One Tap dialog
+
+var URL_BASE = "http://localhost:8080/"
+
+function saveBoletim(){
+    //captura os dados do form, já colocando como um JSON
+    dados = $('#tipo_boletim, #cep_boletim, #cidade_boletim, #estado_boletim,#logradouro_boletim, #bairro_boletim, #desc_boletim, #previsao_boletim').serializeJSON();
+    dados['token_user'] = data.sub;
+
+    console.log(dados);
+
+    //envia para o backend
+    $.ajax(URL_BASE+"boletim",{
+        data:JSON.stringify(dados),
+        method:'post',
+        contentType: "application/json",
+    }).done(function(res) {
+        console.log(res);
+    })
+    .fail(function(res) {
+        console.log(res);
+    });  
+
+}
+
+function saveProblema(){
+    //captura os dados do form, já colocando como um JSON
+    dados = $('#tipo_problema, #cep_problema, #cidade_problema, #estado_problema,#logradouro_problema, #numero_rua_problema, #bairro_problema, #desc_problema').serializeJSON();
+    dados['token_user'] = data.sub;
+
+    console.log(dados);
+
+    //envia para o backend
+    $.ajax(URL_BASE+"problema",{
+        data:JSON.stringify(dados),
+        method:'post',
+        contentType: "application/json",
+    }).done(function(res) {
+
+        let table = $('#tableContent');
+        table.html("");
+        $(res._embedded.boletim).each(function(k,el){
+            let boletim = el;
+            tr = $(`<tr><td>Editar</td><td>Deletar</td></tr>`);
+            table.append(tr);
+        })
+    
+    })
+    .fail(function(res) {
+        console.log(res);
+    });  
+
+}
